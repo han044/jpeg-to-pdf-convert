a3a33844913d25d6be04d89da07bb621485b0578
Rohan Raj <pvrohanraj@gmail.com>
2022-01-12 12:15:14 +0530

    Removed redundant listdir, removed try catch and replaced with if block, used splittext, formatted text strings used instead of concat, removed user input from console

--- JPEGtoPNG.py
+++ JPEGtoPNG.py
@@ -1,27 +1,32 @@
+'''
+Add Any Number of Images in the input folder, it will auto-create "converted" directory and copy converted files into it.
+Note: This will only work when folder is selected. modify accordingly
+'''
+
 import os
 import sys
 from PIL import Image
 
 
 def convertImageToPng():
-    print(" \n\n Converting to png... \n")
-    i = len(os.listdir(new_fold)) + 1
-    for index in os.listdir(ex_fold):
-        img = Image.open("./" + ex_fold + "/" + index)
-        img.save("./new/img"+str(i)+".png")
-        i = i + 1
+    print(" \n\n converting to PNG format ... ")
+    img_list = os.listdir(ex_fold)
+    if(len(img_list) == 0):
+        print("Images not given in the input file...")
+        return
+
+    for index in img_list:
+        img = Image.open(f"./{ex_fold}/{index}")
+        clean_name = os.path.splitext(index)[0]
+        img.save(f"./{new_fold}/{clean_name}-converted.png")
 
 
-try:
-    ex_fold = sys.argv[1]
-    new_fold = sys.argv[2]
-except IndexError as err:
-    print(" \n\n Console Arguments Not Given (existing directory, new directory)... ")
-    exit()
+ex_fold = "input"
+new_fold = "converted"
 
-try:
-    os.mkdir(new_fold)
-except FileExistsError:
-    print("\n\n Folder Already exists! ")
+
+if not os.path.exists(new_fold):
+    print("\n Creating Directory \"converted\" ")
+    os.makedirs(new_fold)
 
 convertImageToPng()
